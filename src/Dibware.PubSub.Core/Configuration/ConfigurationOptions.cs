a3420888a5f5ab185afc8b3bdc71b5df0663591d
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 21:34:58 +0100

    Add `ConfigurationOptions`

---
+++ src/Dibware.PubSub.Core/Configuration/ConfigurationOptions.cs
@@ -0,0 +1,19 @@
+namespace Dibware.PubSub.Core.Extensions;
+
+using System.Reflection;
+
+/// <summary>
+/// Represents configuration options for the PubSub components.
+/// </summary>
+public class ConfigurationOptions
+{
+    /// <summary>
+    /// Set to <see langword="true"/> to register notification handlers. Default is <see langword="true"/>.
+    /// </summary>
+    public bool RegisterNotifications { get; set; } = true;
+
+    /// <summary>
+    /// Gets the list of assemblies to scan for handlers and other components.
+    /// </summary>
+    public List<Assembly> Assemblies { get; } = new();
+}
