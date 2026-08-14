d005462da0c33255e083e5a04e106f61ced45ff0
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 22:09:18 +0100

    Add Add further members to `TypeExtensions`

--- src/Dibware.PubSub.Core/Extensions/TypeExtensions.cs
+++ src/Dibware.PubSub.Core/Extensions/TypeExtensions.cs
@@ -8,6 +8,32 @@ using System;
 public static class TypeExtensions
 {
     /// <summary>
+    /// Determines whether the specified type is awaitable (i.e., a Task, ValueTask, or IAsyncEnumerable).
+    /// </summary>
+    /// <param name="type">
+    /// The type to check for awaitability.
+    /// </param>
+    /// <returns>
+    /// Returns <see langword="true"/> if the specified type is awaitable; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsAwaitable(Type type)
+    {
+        if (type.IsTask() || type.IsValueTask())
+            return true;
+
+        if (type.IsGenericType)
+        {
+            var genericDefinition = type.GetGenericTypeDefinition();
+
+            return genericDefinition.IsTask() ||
+                   genericDefinition.IsValueTask() ||
+                   genericDefinition == typeof(IAsyncEnumerable<>);
+        }
+
+        return false;
+    }
+
+    /// <summary>
     /// Gets a value indicating whether the specified type is a concrete class (i.e., a non-abstract class).
     /// </summary>
     /// <param name="type">
@@ -31,4 +57,32 @@ public static class TypeExtensions
     /// Returns <see langword="true"/> if the specified interface is a generic type and matches the given generic type definition; otherwise, <see langword="false"/>.
     /// </returns>
     public static bool IsGenericInterfaceWhichMatchesType(this Type @interface, Type genericTypeDefinitionToMatch) => @interface.IsGenericType && genericTypeDefinitionToMatch == @interface.GetGenericTypeDefinition();
+
+    /// <summary>
+    /// Determines whether the specified type is a <see cref="Task"/>.
+    /// </summary>
+    /// <param name="type">
+    /// The type to check for being a <see cref="Task"/>.
+    /// </param>
+    /// <returns>
+    /// Returns <see langword="true"/> if the specified type is a <see cref="Task"/>; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsTask(this Type type)
+    {
+        return type == typeof(Task);
+    }
+
+    /// <summary>
+    /// Determines whether the specified type is a <see cref="ValueTask"/>.
+    /// </summary>
+    /// <param name="type">
+    /// The type to check for being a <see cref="ValueTask"/>.
+    /// </param>
+    /// <returns>
+    /// Returns <see langword="true"/> if the specified type is a <see cref="ValueTask"/>; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsValueTask(this Type type)
+    {
+        return type == typeof(ValueTask);
+    }
 }
