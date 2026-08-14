95bd07b82276961999de59b86b7020d47d04b907
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 06:19:09 +0100

    Add `INotification`

---
+++ src/Dibware.PubSub.Core/Contracts/INotification.cs
@@ -0,0 +1,7 @@
+namespace Dibware.PubSub.Core.Contracts;
+
+/// <summary>
+/// Represents a notification that can be published to subscribers. 
+/// This interface serves as a marker for notification types.
+/// </summary>
+public interface INotification { }
