e2930ab9fee719f73a193b08c3eed3d4f6f125b8
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 21:52:57 +0100

    Add `IsGenericInterfaceWhichMatchesType(type, Type)` to `TypeExtensions`

--- src/Dibware.PubSub.Core/Extensions/TypeExtensions.cs
+++ src/Dibware.PubSub.Core/Extensions/TypeExtensions.cs
@@ -17,4 +17,18 @@ public static class TypeExtensions
     /// Returns <see langword="true"/> if the specified type is a concrete class; otherwise, <see langword="false"/>.
     /// </returns>
     public static bool IsConcreteClass(this Type type) => type.IsClass && !type.IsAbstract;
+
+    /// <summary>
+    /// Determines whether the specified interface is a generic type and matches the given generic type definition.
+    /// </summary>
+    /// <param name="interface">
+    /// The interface type to check.
+    /// </param>
+    /// <param name="genericTypeDefinitionToMatch">
+    /// The generic type definition to match.
+    /// </param>
+    /// <returns>
+    /// Returns <see langword="true"/> if the specified interface is a generic type and matches the given generic type definition; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsGenericInterfaceWhichMatchesType(this Type @interface, Type genericTypeDefinitionToMatch) => @interface.IsGenericType && genericTypeDefinitionToMatch == @interface.GetGenericTypeDefinition();
 }
