f7ef2fbf0e5f19c161281890881ac53c60800267
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 21:31:54 +0100

    Update `SimpleMediatorTests` to include test for three identical subcriptions to the same event.

--- tests/Dibware.PubSub.Core.UnitTests/Tests/SimpleMediatorTests.cs
+++ tests/Dibware.PubSub.Core.UnitTests/Tests/SimpleMediatorTests.cs
@@ -1,6 +1,7 @@
 namespace Dibware.PubSub.Core.UnitTests.Tests;
 
 using Dibware.PubSub.Core.Contracts;
+using Dibware.PubSub.Core.Extensions;
 using Dibware.PubSub.Core.UnitTests.Fakes.Events;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -13,6 +14,10 @@ public sealed class SimpleMediatorTests
     {
         // Arrange
         var services = new ServiceCollection();
+        services.AddSimpleMediator(options =>
+        {
+            options.RegisterNotifications = true;
+        });
 
         var notificationHandlerMock = new Mock<INotificationHandler<UserRegisteredEvent>>();
 
@@ -23,7 +28,7 @@ public sealed class SimpleMediatorTests
         _ = services.AddScoped<INotificationHandler<UserRegisteredEvent>>(provider => notificationHandlerMock.Object);
 
         var serviceProvider = services.BuildServiceProvider();
-        var mediator = serviceProvider.GetRequiredService<IPublisher<UserRegisteredEvent>>();
+        var mediator = serviceProvider.GetRequiredService<ISimpleMediator>();
         var userRegisteredEvent = new UserRegisteredEvent("testuser", "testemail");
 
         // Act
@@ -31,6 +36,58 @@ public sealed class SimpleMediatorTests
 
         // Assert
         notificationHandlerMock.Verify(
+            handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()),
+            Times.Once()
+        );
+    }
+
+    [TestMethod]
+    public async Task Publish_CallsAllHandlers_WhenThreeHandlersAreRegistered()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddSimpleMediator(options =>
+        {
+            options.RegisterNotifications = true;
+        });
+
+        var notificationHandlerMock1 = new Mock<INotificationHandler<UserRegisteredEvent>>();
+        var notificationHandlerMock2 = new Mock<INotificationHandler<UserRegisteredEvent>>();
+        var notificationHandlerMock3 = new Mock<INotificationHandler<UserRegisteredEvent>>();
+
+        notificationHandlerMock1
+            .Setup(handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        notificationHandlerMock2
+            .Setup(handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        notificationHandlerMock3
+            .Setup(handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        _ = services.AddScoped<INotificationHandler<UserRegisteredEvent>>(provider => notificationHandlerMock1.Object);
+        _ = services.AddScoped<INotificationHandler<UserRegisteredEvent>>(provider => notificationHandlerMock2.Object);
+        _ = services.AddScoped<INotificationHandler<UserRegisteredEvent>>(provider => notificationHandlerMock3.Object);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var mediator = serviceProvider.GetRequiredService<ISimpleMediator>();
+        var userRegisteredEvent = new UserRegisteredEvent("testuser", "testemail");
+
+        // Act
+        await mediator.Publish(userRegisteredEvent);
+
+        // Assert
+        notificationHandlerMock1.Verify(
+            handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()),
+            Times.Once()
+        );
+        notificationHandlerMock2.Verify(
+            handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()),
+            Times.Once()
+        );
+        notificationHandlerMock3.Verify(
             handler => handler.Handle(It.IsAny<UserRegisteredEvent>(), It.IsAny<CancellationToken>()),
             Times.Once()
         );
