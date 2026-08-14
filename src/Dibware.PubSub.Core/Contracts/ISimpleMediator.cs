571ae4973f152cb712c48c080e19a74a75fe8413
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 21:36:15 +0100

    Update `ISimpleMediator` interface

--- src/Dibware.PubSub.Core/Contracts/ISimpleMediator.cs
+++ src/Dibware.PubSub.Core/Contracts/ISimpleMediator.cs
@@ -2,10 +2,8 @@ namespace Dibware.PubSub.Core.Contracts;
 
 /// <summary>
 /// Defines the expected members of a simple mediator that can publish notifications to subscribers.
-/// </summary>
-/// <typeparam name="TNotification">
-/// The type of notification that the mediator can publish. Must implement the <see cref="INotification"/> interface.
-/// </typeparam>
-public interface ISimpleMediator<TNotification> : IPublisher<TNotification>
-    where TNotification : INotification
+/// Currently this is only used for publishing notifications, but could be extended to support
+/// requests and responses in the future.
+/// </summary>>
+public interface ISimpleMediator : IPublisher 
 { }
