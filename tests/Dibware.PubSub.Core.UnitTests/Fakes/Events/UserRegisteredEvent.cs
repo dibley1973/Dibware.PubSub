e84a82f988f412de2099c5a5e3f2249f7f87eb85
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-14 06:28:55 +0100

    Clean up `UserRegisteredEvent`

--- tests/Dibware.PubSub.Core.UnitTests/Fakes/Events/UserRegisteredEvent.cs
+++ tests/Dibware.PubSub.Core.UnitTests/Fakes/Events/UserRegisteredEvent.cs
@@ -1,11 +1,10 @@
-﻿using Dibware.PubSub.Core.Contracts;
+namespace Dibware.PubSub.Core.UnitTests.Fakes.Events;
 
-namespace Dibware.PubSub.Core.UnitTests.Fakes.Events
-{
-    /// <summary>
-    /// Represents a fake event that is used to test the activity log handler.
-    /// </summary>
-    /// <param name="UserName">Represents the user name.</param>
-    /// <param name="Email">Represents the email address.</param>
-    public record UserRegisteredEvent(string UserName, string Email) : INotification;
-}
+using Dibware.PubSub.Core.Contracts;
+
+/// <summary>
+/// Represents a fake event that is used to test the activity log handler.
+/// </summary>
+/// <param name="UserName">Represents the user name.</param>
+/// <param name="Email">Represents the email address.</param>
+public record UserRegisteredEvent(string UserName, string Email) : INotification;
