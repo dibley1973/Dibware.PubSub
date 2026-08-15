aa2ce929c40ac9d74e86748a6959c9b514bd58f7
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-15 05:56:39 +0100

    Add Further XML documetation to `CancellationTokenExtensions`

--- src/Dibware.PubSub.Core/Extensions/CancellationTokenExtensions.cs
+++ src/Dibware.PubSub.Core/Extensions/CancellationTokenExtensions.cs
@@ -1,5 +1,8 @@
 namespace Dibware.PubSub.Core.Contracts;
 
+/// <summary>
+/// Extension methods for the <see cref="CancellationToken"/> object.
+/// </summary>
 public static class CancellationTokenExtensions
 {
     /// <summary>
@@ -11,6 +14,13 @@ public static class CancellationTokenExtensions
     /// <returns>
     /// A Task that completes when the CancellationToken is canceled.
     /// </returns>
+    /// <remarks>
+    /// This code is courtesy of StephenCleary and his anser to a stack overflow question.
+    /// REF: https://stackoverflow.com/a/27240225/254215
+    /// 
+    /// The original code can be found at the following link:
+    /// REf: https://github.com/StephenCleary/AsyncEx/wiki/CancellationTokenExtensions
+    /// </remarks>
     public static Task AsTask(this CancellationToken cancellationToken)
     {
         var tcs = new TaskCompletionSource<object>();
