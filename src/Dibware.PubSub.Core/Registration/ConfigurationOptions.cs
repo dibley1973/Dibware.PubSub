d75f1e61c9754b1e09716bd63c1f1f534c07e3a6
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-15 21:45:33 +0100

    Change boolean property `RegisterNotificationsFromAssemblies` in `ConfigurationOptions` to an enum `NotificationRegistrationMode` to indicate the behaviour.

--- src/Dibware.PubSub.Core/Registration/ConfigurationOptions.cs
+++ src/Dibware.PubSub.Core/Registration/ConfigurationOptions.cs
@@ -8,19 +8,42 @@ using System.Reflection;
 public class ConfigurationOptions
 {
     /// <summary>
-    /// Set to <see langword="true"/> to register notification handlers from assemblies. Default is <see langword="false"/>.
-    /// Ensure if this property is set to <see langword="true"/>, that the <see cref="AssembliesToScanForNotifications"/>
-    /// property is populated with the assemblies containing the notification handlers.
-    /// </summary>
-    public bool RegisterNotificationsFromAssemblies { get; set; } = false;
-
-    /// <summary>
-    /// Gets the list of assemblies to scan for handlers and other components.
-    /// Ensure that <see cref="RegisterNotificationsFromAssemblies"/> is set to <see langword="true"/>,
+    /// Gets or sets the list of assemblies to scan for handlers and other components.
+    /// Ensure that <see cref="NotificationRegistrationMode"/> is set to <see cref="NotificationRegistrationMode.FromAssemblies"/>,
     /// if assemblies containing the notification handlers are included in this list.
     /// Defaults to an empty list.
     /// </summary>
     public List<Assembly> AssembliesToScanForNotifications { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the list of types to register for notifications.
+    /// Ensure that <see cref="NotificationRegistrationMode"/> is set to  <see cref="NotificationRegistrationMode.FromTypes"/>,
+    /// if types which are the registered as notification handlers are included in this list.
+    /// Defaults to an empty list.
+    /// </summary>
+    public List<Type> TypesToRegisterForNotifications { get; set; } = new();
+
+    /// <summary>
+    /// Gets or sets the mode of registration for notification handlers, specifying how they should be discovered and registered within the system.
+    /// The default is <see cref="NotificationRegistrationMode.ManualRegistration"/>.
+    /// <para>
+    /// This property allows you to control the registration behavior of notification handlers,
+    /// enabling you to choose between manual registration, assembly scanning, or type-based registration.
+    /// </para>
+    /// <para>
+    /// When set to <see cref="NotificationRegistrationMode.ManualRegistration"/>, the system will not automatically
+    /// register handlers from assemblies or types, and you will need to handle registration manually.
+    /// </para>
+    /// <para>
+    /// When set to <see cref="NotificationRegistrationMode.FromAssemblies"/>, the system will scan the assemblies specified
+    /// in <see cref="AssembliesToScanForNotifications"/> for notification handlers and register them automatically.
+    /// </para>
+    /// <para>
+    /// When set to <see cref="NotificationRegistrationMode.FromTypes"/>, the system will register the types specified
+    /// in <see cref="TypesToRegisterForNotifications"/> as notification handlers automatically.
+    /// </para>
+    /// </summary>
+    public NotificationRegistrationMode NotificationRegistrationMode { get; set; } = NotificationRegistrationMode.ManualRegistration;
 
     /// <summary>
     /// Gets or sets the processing mode for notification publishers. 
