6dcdd767c5b58f0dfee95c3c515d3a1fef750bc1
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-16 12:09:50 +0100

    Comment out `SecondFakeUserUnRegisteredEventNotificationHandler` and leave a note why

--- tests/Dibware.PubSub.Core.UnitTests/Fakes/Handlers/SecondFakeUserUnRegisteredEventNotificationHandler.cs
+++ tests/Dibware.PubSub.Core.UnitTests/Fakes/Handlers/SecondFakeUserUnRegisteredEventNotificationHandler.cs
@@ -1,33 +1,35 @@
-namespace Dibware.PubSub.Core.UnitTests.Fakes.Handlers;
+// TODO: Uncomment for this test "AddSimpleMediator_ThrowsExceptionWhen_WhentwoHandlersForSameEventAreAutomaticallyRegistered"
 
-using Dibware.PubSub.Core.Contracts;
-using Dibware.PubSub.Core.UnitTests.Fakes.Events;
+//namespace Dibware.PubSub.Core.UnitTests.Fakes.Handlers;
 
-/// <summary>
-/// Represents a second fake notification handler for the UserUnRegisteredEvent used in unit tests.
-/// Once the Handle method is called, the HandleCalled property will be set to true.
-/// </summary>
-public class SecondFakeUserUnRegisteredEventNotificationHandler : INotificationHandler<UserUnRegisteredEvent>
-{
-    /// <summary>
-    /// Indicates whether the Handle method has been called. This property starts as <see langword="false"/>,
-    /// and is set to <see langword="true"/> when the Handle method is invoked.
-    /// </summary>
-    public bool HandleCalled { get; private set; } = false;
+//using Dibware.PubSub.Core.Contracts;
+//using Dibware.PubSub.Core.UnitTests.Fakes.Events;
 
-    /// <summary>
-    /// Handles the UserUnRegisteredEvent notification. When this method is called, it sets the HandleCalled property to <see langword="true"/>.
-    /// </summary>
-    /// <param name="_notification">
-    /// The UserUnRegisteredEvent notification that is being handled. This parameter is not used in the method,
-    /// but it is required by the INotificationHandler interface.
-    /// </param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public Task Handle(UserUnRegisteredEvent _notification, CancellationToken cancellationToken)
-    {
-        HandleCalled = true;
+///// <summary>
+///// Represents a second fake notification handler for the UserUnRegisteredEvent used in unit tests.
+///// Once the Handle method is called, the HandleCalled property will be set to true.
+///// </summary>
+//public class SecondFakeUserUnRegisteredEventNotificationHandler : INotificationHandler<UserUnRegisteredEvent>
+//{
+//    /// <summary>
+//    /// Indicates whether the Handle method has been called. This property starts as <see langword="false"/>,
+//    /// and is set to <see langword="true"/> when the Handle method is invoked.
+//    /// </summary>
+//    public bool HandleCalled { get; private set; } = false;
 
-        return Task.CompletedTask;
-    }
-}
+//    /// <summary>
+//    /// Handles the UserUnRegisteredEvent notification. When this method is called, it sets the HandleCalled property to <see langword="true"/>.
+//    /// </summary>
+//    /// <param name="_notification">
+//    /// The UserUnRegisteredEvent notification that is being handled. This parameter is not used in the method,
+//    /// but it is required by the INotificationHandler interface.
+//    /// </param>
+//    /// <param name="cancellationToken"></param>
+//    /// <returns></returns>
+//    public Task Handle(UserUnRegisteredEvent _notification, CancellationToken cancellationToken)
+//    {
+//        HandleCalled = true;
+
+//        return Task.CompletedTask;
+//    }
+//}
