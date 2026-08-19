3676e6b30fd36aec03b2f717643ac8aae8a483a4
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-19 06:03:22 +0100

    Flesh-out `ISubscriber` member signatures

--- src/Dibware.PubSub.Core/Contracts/ISubscriber.cs
+++ src/Dibware.PubSub.Core/Contracts/ISubscriber.cs
@@ -3,4 +3,29 @@ namespace Dibware.PubSub.Core.Contracts;
 /// <summary>
 /// Defines the expected members of a component which takes subsriptions.
 /// </summary>
-public interface ISubscriber;
+public interface ISubscriber
+{
+    /// <summary>
+    /// Subscribes the specified handler.
+    /// </summary>
+    /// <typeparam name="TNotification">
+    /// The type of the notification. Must implement <see cref="INotification"/>.
+    /// </typeparam>
+    /// <param name="handler">
+    /// The handler to subscribe.
+    /// </param>
+    public void Subscribe<TNotification>(INotificationHandler<TNotification> handler)
+        where TNotification : INotification;
+
+    /// <summary>
+    /// Unsubscribes the specified handler.
+    /// </summary>
+    /// <typeparam name="TNotification">
+    /// The type of the notification. Must implement <see cref="INotification"/>.
+    /// </typeparam>
+    /// <param name="handler">
+    /// The handler to unsubscribe.
+    /// </param>
+    public void Unsubscribe<TNotification>(INotificationHandler<TNotification> handler)
+        where TNotification : INotification;
+}
