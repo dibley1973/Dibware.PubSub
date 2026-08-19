b806593e519236e4fe29df4e313aa02f8dc8b4ac
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-19 06:05:01 +0100

    Flesh out unit test `Publish_CallsSingleHandler_WhenSingleHandlerIsManuallyRegistered`

--- tests/Dibware.PubSub.Core.UnitTests/Tests/MultiSubPubTests/MultiSubPub_HandlersBoundManuallyByClass.cs
+++ tests/Dibware.PubSub.Core.UnitTests/Tests/MultiSubPubTests/MultiSubPub_HandlersBoundManuallyByClass.cs
@@ -1,6 +1,9 @@
 namespace Dibware.PubSub.Core.UnitTests.Tests.MultiSubPubTests;
 
+using Dibware.PubSub.Core.Contracts;
 using Dibware.PubSub.Core.Extensions;
+using Dibware.PubSub.Core.UnitTests.Fakes.Events;
+using Dibware.PubSub.Core.UnitTests.Fakes.Handlers;
 using Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
@@ -12,10 +15,26 @@ public sealed class MultiSubPub_HandlersBoundManuallyByClass
     [TestMethod]
     public async Task Publish_CallsSingleHandler_WhenSingleHandlerIsManuallyRegistered()
     {
-
         // Arrange
         var services = new ServiceCollection();
         AddMultiSubPubWithDefaultOptions(services);
+        var serviceProvider = services.BuildServiceProvider();
+        var pubSub = serviceProvider.GetRequiredService<ISubscribeAndPublish>();
+        var userRegisteredEvent = new UserRegisteredEvent("test-user", "email-address");
+        var userUnRegisteredEventNotificationHandler =
+            (FakeUserRegisteredEventNotificationHandler)serviceProvider.GetRequiredService<INotificationHandler<UserRegisteredEvent>>();
+
+        pubSub.Subscribe(userUnRegisteredEventNotificationHandler);
+
+        // Act
+        await pubSub.Publish(userRegisteredEvent);
+
+        // Assert
+        Assert.IsNotNull(userUnRegisteredEventNotificationHandler);
+        Assert.IsTrue(userUnRegisteredEventNotificationHandler.HandleCalled);
+
+        // Clean up
+        pubSub.Unsubscribe(userUnRegisteredEventNotificationHandler);
     }
 
     private void AddMultiSubPubWithDefaultOptions(ServiceCollection services)
