a7cf900aa076389a71b9f9b81a7e510ebc792e97
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-19 06:13:50 +0100

    Add `TestContext` to `SimpleMediatorTests_HandlersBoundAutomaticallyByTypes` unit tests

--- tests/Dibware.PubSub.Core.UnitTests/Tests/SimpleMediatorTests/SimpleMediatorTests_HandlersBoundAutomaticallyByTypes.cs
+++ tests/Dibware.PubSub.Core.UnitTests/Tests/SimpleMediatorTests/SimpleMediatorTests_HandlersBoundAutomaticallyByTypes.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 [TestClass]
 public sealed class SimpleMediatorTests_HandlersBoundAutomaticallyByTypes
 {
+    public TestContext TestContext { get; set; }
+
     [TestMethod]
     public async Task Publish_CallsSingleHandler_WhenSingleHandlerIsAutomaticallyRegistered()
     {
@@ -24,7 +26,7 @@ public sealed class SimpleMediatorTests_HandlersBoundAutomaticallyByTypes
             (FakeUserUnRegisteredEventNotificationHandler)serviceProvider.GetRequiredService<INotificationHandler<UserUnRegisteredEvent>>();
 
         // Act
-        await mediator.Publish(userRegisteredEvent);
+        await mediator.Publish(userRegisteredEvent, TestContext.CancellationToken);
 
         // Assert
         Assert.IsNotNull(userUnRegisteredEventNotificationHandler);
