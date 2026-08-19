a9414b037e370cb1dbdd0e85935e6f0ac4aa47d3
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-19 06:03:58 +0100

    Flesh-out `MultiSubPub` member signatures to match `ISubscriber`

--- src/Dibware.PubSub.Core/MultiSubPub.cs
+++ src/Dibware.PubSub.Core/MultiSubPub.cs
@@ -10,5 +10,62 @@ using Dibware.PubSub.Core.Contracts;
 /// </summary>
 public class MultiSubPub : ISubscribeAndPublish
 {
-    public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification => throw new NotImplementedException();
+    /// <summary>
+    /// Asynchonously publishes a notification to all subscribers of the specified event type.
+    /// </summary>
+    /// <typeparam name="TNotification">
+    /// The type of notification to publish. Must implement <see cref="INotification"/>.
+    /// </typeparam>
+    /// <param name="notification">The actual notification to publish.</param>
+    /// <param name="cancellationToken">An optional cancellation token</param>
+    /// <returns>A task that represents the publish operation.</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : INotification
+    {
+        ArgumentNullException.ThrowIfNull(notification, nameof(notification));
+
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Subscribes the specified handler.
+    /// </summary>
+    /// <typeparam name="TNotification">
+    /// The type of the notification. Must implement <see cref="INotification"/>.
+    /// </typeparam>
+    /// <param name="handler">
+    /// The handler to subscribe.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if the value of <paramref name="handler"/> is <see langword="null"/>.
+    /// </exception>
+    public void Subscribe<TNotification>(INotificationHandler<TNotification> handler)
+        where TNotification : INotification
+    {
+        ArgumentNullException.ThrowIfNull(handler);
+
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Unsubscribes the specified handler.
+    /// </summary>
+    /// <typeparam name="TNotification">
+    /// The type of the notification. Must implement <see cref="INotification"/>.
+    /// </typeparam>
+    /// <param name="handler">
+    /// The handler to unsubscribe.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if the value of <paramref name="handler"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="NotImplementedException"></exception>
+    public void Unsubscribe<TNotification>(INotificationHandler<TNotification> handler)
+        where TNotification : INotification
+    {
+        ArgumentNullException.ThrowIfNull(handler);
+
+        throw new NotImplementedException();
+    }
 }
